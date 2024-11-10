43f6d86084661cfb735e6580a587f775f20f296b
rajendrachy <chyrajendra32@gmail.com>
2024-11-10 23:16:01 +0530

    change update link mcq

---
+++ PANDAS IN PYTHON/MCQ _LINK/link.py
@@ -0,0 +1,7 @@
+# _________Pandas________________
+
+# link -> https://chatgpt.com/share/6730ed3a-e808-800b-b729-f754aa991acf
+
+# _________Explotary______________
+
+# link -> https://chatgpt.com/share/6730eee3-8608-800b-8e99-888cd5effad4
